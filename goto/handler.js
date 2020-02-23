ab21750b1df18c8bd5e88ac86c275165004733dd
Kianoosh Hooshmand <kianoosh.hooshmand@gmail.com>
2020-02-23 16:24:22 +0330

    formatting

--- goto/handler.js
+++ goto/handler.js
@@ -3,21 +3,21 @@
 module.exports = (event, context) => {
     let redirect;
 
-    
+
     /* Use a switch statement, or look up the routes in a database
      * such as MongoDB, Redis or Postgres. AWS and DigitalOcean provide
      * remote, managed databases that would work here and maintain
      * a connection pool */
-    if(event.path == "/home") {
+    if (event.path == "/home") {
         redirect = "https://www.alexellis.io/";
-    } else if(event.path == "/sponsors" || event.path == "/insiders") {
+    } else if (event.path == "/sponsors" || event.path == "/insiders") {
         redirect = "https://github.com/users/alexellis/sponsorship";
     }
 
     /* Let the user know we couldn't find the URL, we could
      * also return a HTML page and set the correct encoding for the
      * browser to understand. */
-    if(!redirect) {
+    if (!redirect) {
         return context
             .status(400)
             .fail("Unknown short URL");
@@ -28,6 +28,6 @@ module.exports = (event, context) => {
      * https://en.wikipedia.org/wiki/HTTP_302 */
     context
         .status(302)
-        .headers({"location": redirect})
+        .headers({ "location": redirect })
         .succeed();
 }
